e9d7a02ad4ab172855ad574e5f1b967497619316
Manoel Wagner <42157743+mwmcontador@users.noreply.github.com>
2021-05-31 00:58:06 +0100

    begin 2

--- ws/src/services/database.js
+++ ws/src/services/database.js
@@ -1,9 +1,10 @@
-const mongoose = require("mongoose");
-
+// Mongoose é a biblioteca para manipular o Mongodb com javascript
+//##### Exemplo da blibioteca
 //'mongodb://localhost:27017/nome_da_base_de_dados
 //mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
 
-mongoose.connect("mongodb://localhost:27017/netflix", {
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/secclass-teste", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
