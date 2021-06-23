2388c6f9338b0bbeead988d39179db3328cba6b1
Manoel Wagner <42157743+mwmcontador@users.noreply.github.com>
2021-06-23 22:26:28 +0100

    v1

--- ws/src/routes/item.routes.js
+++ ws/src/routes/item.routes.js
@@ -5,7 +5,7 @@ const Item = require("../model/item");
 //
 console.log("Começando a Lista Item");
 //Visializacao de Todas as Tabelas
-router.get("/lista", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     console.log("Iniciando rota do item");
 
@@ -15,6 +15,51 @@ router.get("/lista", async (req, res) => {
     console.log("Itens Lidos");
   } catch (err) {
     console.log("Error Item");
+    res.json({ error: true, message: err.message });
+  }
+});
+
+//Listando Somente um ID
+router.get("/lista/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    //Debug
+    console.log(`Iniciando ${id}`);
+    const item = await Item.findById(id);
+    res.json({ error: false, item });
+  } catch (err) {
+    res.json({ error: true, message: err.message });
+  }
+});
+
+//Listando Somente um Registro pelo Nivel
+router.get("/filtros/:nivel_item&&", async (req, res) => {
+  console.log(`Iniciando Nivel ${req.params.nivel_item}`);
+  try {
+    const nivel = req.params.nivel_item;
+    const nome_tabela = req.params.nivel_item;
+
+    //Debug
+    console.log(` Nivel Selecionado ${nivel}`);
+    const item = await Item.find({ nivel_item: nivel });
+    res.json({ error: false, item });
+  } catch (err) {
+    res.json({ error: true, message: err.message });
+  }
+});
+
+//Listando Somente com os filtros
+router.get("/nivel/:nivel_item", async (req, res) => {
+  console.log(`Iniciando Nivel ${req.params.nivel_item}`);
+  try {
+    const criterio = req.params.nivel_item;
+
+    //Debug
+    console.log(` Nivel Selecionado ${criterio}`);
+    const item = await Item.find({ nivel_item: criterio });
+    res.json({ error: false, item });
+  } catch (err) {
     res.json({ error: true, message: err.message });
   }
 });
