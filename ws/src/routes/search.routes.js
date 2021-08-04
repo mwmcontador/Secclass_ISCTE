d568d458ddae85f25f9935e1f25777f4ef66a7e7
ddces-iscteiulpt <ddces@iscte-iul.pt>
2021-08-04 16:55:47 +0100

    API Search + Filtros - OK

--- ws/src/routes/search.routes.js
+++ ws/src/routes/search.routes.js
@@ -3,56 +3,61 @@ const router = express.Router();
 const Search = require("../model/pesquisas");
 const Item = require("../model/item");
 
-/*
-//////////// POST API - USER INPUT
-//var input_pesquisa = "arr";
-//var input_pesquisa = "?";
-var input_pesquisa = "arrefecimento";
-var input =
-router.get("/search/" + ${input_pesquisa}, async (req, res) => {
-  try {
 
-  }  catch (err) {
-    console.log("Error Item");
-    res.json({ error: true, message: err.message });
-  }
-});
-
-console.log(input_pesquisa);
-*/
-//class PesquisaController {
 //////////// GET API - PESQUISA
-  router.get("/search", async (req, res) => {
+  router.get("/search/:input_pesquisa/:criterio_tabela/:criterio_nivel", async (req, res) => {
     try {
+      const input_pesquisa = req.params.input_pesquisa;
+      const criterio_tabela = req.params.criterio_tabela;
+      const criterio_nivel = req.params.criterio_nivel;
 
-    if(input_pesquisa == "?"){
-      req = '.*\\' + input_pesquisa + '.*';
+      console.log(
+        `SearchRout: ${input_pesquisa} , ${criterio_tabela} e ${criterio_nivel}`
+      );
+
+    var search;
+
+    if(input_pesquisa == "(?)"){
+      search = '.*\\' + input_pesquisa + '.*';
     }
     else {
-      req = '.*' + input_pesquisa + '.*';
+      search = '.*' + input_pesquisa + '.*';
     }
-
-    console.log(req);
+    console.log(search , input_pesquisa, criterio_nivel);
     const data_out = await Item.find({
       $or: [
-        {"code_item": { "$regex": req, "$options": "i"} },
-        {"titulo_SECClasS": { "$regex": req, "$options": "i"} }
-      ]
+        {$and: [
+          {"code_item": { "$regex": search, "$options": "i"} },
+          {"nivel_item":  criterio_nivel },
+          {"code_tabela": criterio_tabela}
+        ]},
+        {$and: [
+        {"titulo_SECClasS": { "$regex": search, "$options": "i"} },
+        {"nivel_item":  criterio_nivel },
+        {"code_tabela": criterio_tabela}
+      ]}
+    ]}, function(err){
+      // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+      if (err)
+      {
+        res.send(err);
+        console.log(`err: ${err}`)
+      }
     });
+    //console.log(`Numeros de docs: ${length(data_out)}.`);
+    console.log(`Data_ou = ${data_out}`);
 
     const store = {
-      "Users_id": "61014705970082f592719864",
+      "Users_id": "61014705970082f592719864",  //ID Public User
       "pesquisa_txt": input_pesquisa,
       "resultados": "",
-      "Timestamp": new Date()
+      "Timestamp": new Date()                 //current date to timestamp
     };
     console.log(store);
+    const data_save = await Search.create(store);
+    console.log(data_save);
 
-    const data_input = await Search.create(store);
-
-    console.log(data_input);
-
-
+    //Debug
     res.json({ error: false, data_out});
   }  catch (err) {
     console.log("Error Item");
@@ -61,10 +66,7 @@ console.log(input_pesquisa);
 });
 
 
-//}
-//module.exports = new PesquisaController();
-
-
+////////////////////////////////////////////////////////////////////////////////
 //Listando Somente com os filtros
 router.get("/filtros/", async (req, res) => {
   try {
