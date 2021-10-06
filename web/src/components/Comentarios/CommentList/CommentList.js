91d353ffaac3933cade9d4ce0982d53942ffd53d
Manoel Wagner <42157743+mwmcontador@users.noreply.github.com>
2021-10-06 13:39:47 +0100

    Pesquisa Automatica - Input

--- web/src/components/Comentarios/CommentList/CommentList.js
+++ web/src/components/Comentarios/CommentList/CommentList.js
@@ -4,14 +4,12 @@ const CommentList = ({ comment_id }) => {
   console.log("Comentario", { comment_id });
 
   return (
-    <tr className="table table-hover">
-      <td type="date" className="col">
-        {comment_id.timestamp}
-      </td>
+    <tr className="table table-striped">
+      <td>{comment_id.date_string}</td>
       <td>{comment_id.comment}</td>
       <td>{comment_id.name}</td>
       <td>{comment_id.institution}</td>
-      <td>{comment_id.status}</td>
+      {/*<td>{comment_id.status}</td>*/}
     </tr>
   );
 };
