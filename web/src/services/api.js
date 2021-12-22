3588f2580b091d82ae3c2bbc3a9bf62cc4cf0912
ddces-iscteiulpt <ddces@iscte-iul.pt>
2021-12-22 16:18:33 +0000

    52 - webapp online new server

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 console.log("acessando a API");
 const api = axios.create({
   //baseURL: "http://localhost:5003/",
-  baseURL: "http://193.136.189.87:5003/",
+  //baseURL: "http://193.136.189.87:5003/",
+  baseURL: "http://217.112.93.248:5003/",
 });
 console.log("Rodando a API");
 export default api;
