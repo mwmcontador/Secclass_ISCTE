8703d9c5ae1e8042071de375157f2cadbc21929f
dc-inov <daniel.cale@inov.pt>
2022-03-26 18:55:25 +0000

    cluster0 update

--- ws/src/services/database.js
+++ ws/src/services/database.js
@@ -3,8 +3,8 @@
 //'mongodb://localhost:27017/nome_da_base_de_dados
 //mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
 const mongoose = require("mongoose");
-const uri = "mongodb+srv://root_out:client_top@cluster0.rfblu.mongodb.net/SecClasS?retryWrites=true&w=majority";
-const localhostMongoDB = "mongodb://localhost:27017/SECClasS-DB";
+const uri = "mongodb+srv://Admin_mongo:KwruOypOOnxV29pP@cluster0.y0okk.mongodb.net/SECClasS-DB?retryWrites=true&w=majority";
+const localhostMongoDB = "mongodb://217.112.93.248:27017/SECClasS-DB";
 //console.log(`URI MongoDB: ${uri}`);
 
 var DB_backup;
@@ -28,7 +28,7 @@ var options1 = {
 
 console.log("Connecting DATABASE.........");
 mongoose.connect(uri, options1).then(
-  () => { console.log(".........LocalhostDB Connected") /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
+  () => { console.log(".........Cluster0 Connected") /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
   err => { DB_backup = 0, console.log(`MongoDB err: ${err}`)/** handle initial connection error */ }
 );
 
